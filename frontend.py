17659a0304f268f2f14f3a882b1f82a54839992e
gkm05 <er.gkm05@gmail.com>
2025-01-31 22:29:11 +0530

    updated as per the backend server

--- frontend.py
+++ frontend.py
@@ -21,7 +21,7 @@ allow_web_search=st.checkbox("Allow Web Search")
 
 user_query=st.text_area("Enter your query: ", height=150, placeholder="Ask Anything!")
 
-API_URL="http://0.0.0.0:8000/chat"
+API_URL="https://agentic-chatbot-3tjw.onrender.com/chat"
 
 if st.button("Ask Agent!"):
     if user_query.strip():
